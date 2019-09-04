761ff1d475a35c6dc94d914a12267ea8f427c8ef
ugurtan <pektas.ugurtan@gmail.com>
2019-09-04 16:36:08 +0300

    ilk commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown, faMinusCircle } from '@fortawesome/free-solid-svg-icons'
+import jsonData from './dataset.json'
+import jQuery from 'jquery';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      cityData : []
+    }
+  }
+  componentDidMount(){
+    this.changeData();
+  }
+
+  findChild = (arrayData) => {
+    let newData = []
+    arrayData.forEach(element => {
+      let subData = []
+      subData = arrayData.filter(subElement => subElement.hasOwnProperty('parentID') && subElement.parentID === element.ID)
+      if(subData.length > 0){
+        element['subData'] = subData
+        subData.forEach(subDataElement => {
+          arrayData = arrayData.filter(arrayDataElement => subDataElement.ID !== arrayDataElement.ID)
+        })
+        this.findChild(element.subData)
+      }
+    });
+    newData = arrayData
+    return newData
+  }
+
+  changeData = () => {
+    let newCityData = this.findChild(jsonData)
+    console.log(newCityData)
+    this.setState({
+      cityData:newCityData
+    })
+  }
+  deleteItem = (id) => {
+    this.setState({
+      cityData:this.state.cityData.filter(cityElement => cityElement.ID !== id)
+    })
+  }
+  toggleItem = (id) => {
+    jQuery('#subItem'+id).slideToggle();
+  }
+
+  showSubItems = (city) => {
+    if(city.hasOwnProperty('subData') && city.subData.length > 0){
+      return(
+        <div className="city-sub-container" id={'subItem'+city.ID}>
+          {
+            city.subData.map((data, dataIndex) => {
+              return (
+                <div className="city-item" key={dataIndex}>
+                  {data.Name}
+                  {this.showSubItems(data)}
+                </div>
+              )
+            })
+          }
+        </div>
+      )
+    }
+  }
+  render(){
+    return(
+      <div className="city-container">
+        {
+          this.state.cityData.map((city, cityIndex) => {
+            return (
+            <div className="city-item" key={cityIndex}>
+              <span className="collapse-container" onClick={(e) => this.toggleItem(city.ID, e)}> <FontAwesomeIcon icon={faChevronDown} /> {city.Name}</span> 
+              <span className="delete-container"><a href="#" onClick={(e) => this.deleteItem(city.ID, e)}><FontAwesomeIcon icon={faMinusCircle} /></a></span>
+              {this.showSubItems(city)}
+            </div>)
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
